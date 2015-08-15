6166f68453ef0af83052571d8b10bceccaabc3f0
Federico Bricker <fbricker@gmail.com>
2015-08-15 19:55:57 -0300

    Move amazon functions to common.php file

--- iCanLocalize/common.php
+++ iCanLocalize/common.php
@@ -16,8 +16,45 @@ function buildMap($dom,$langMap){
 	return $res;
 }
 
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
-function buildLeaderBoard($id,$singularID,$pluralID,$data){
+function buildAmazonLeaderboards(){
+	global $data, $leaderboards;
+	$res='';
+	$res = 'buildAmazonLeaderboards: PENDING IMPLEMENTATION!'."\n\n";
+	return $res;
+}
+
+function buildAmazonAchievements(){
+	global $data, $achievements;
+	$res='';
+	//DisplayTitle_en_US,UnlockedDescription_en_US,LockedDescription_en_US,DisplayTitle_en_GB,UnlockedDescription_en_GB,LockedDescription_en_GB,DisplayTitle_de_DE,UnlockedDescription_de_DE,LockedDescription_de_DE,DisplayTitle_fr_FR,UnlockedDescription_fr_FR,LockedDescription_fr_FR,DisplayTitle_it_IT,UnlockedDescription_it_IT,LockedDescription_it_IT,DisplayTitle_es_ES,UnlockedDescription_es_ES,LockedDescription_es_ES,DisplayTitle_ja_JP,UnlockedDescription_ja_JP,LockedDescription_ja_JP,DisplayTitle_zh_CN,UnlockedDescription_zh_CN,LockedDescription_zh_CN,DisplayTitle_ko_KR,UnlockedDescription_ko_KR,LockedDescription_ko_KR,DisplayTitle_pt_BR,UnlockedDescription_pt_BR,LockedDescription_pt_BR,DisplayTitle_ru_RU,UnlockedDescription_ru_RU,LockedDescription_ru_RU'."\n";
+	$res='AchievementId,unlockedIconId,lockedIconId,pointValue,isHidden';
+	foreach($data[$achievements[0]['title_id']] as $lang=>$v){
+		$res.=',DisplayTitle_'.$lang.',UnlockedDescription_'.$lang.',LockedDescription_'.$lang;
+	}
+
+	//AchievementID,UnlockedIconId,LockedIconId,50,TRUE,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description
+	foreach($achievements as $ach){
+	//	if(!empty($data[$ach['id']])) ;
+		$res .= "\n".$ach['id'].','.$ach['icon'].','.$ach['icon'].','.$ach['points'].','.($ach['hidden']?'TRUE':'FALSE');
+		foreach($data[$ach['title_id']] as $lang=>$title){
+			$locked = $data[$ach['description_id']][$lang];
+			$unlocked = $data[$ach['unlocked_id']][$lang];
+			$res.=',"'.$title.'","'.$unlocked.'","'.$locked.'"'; // add to every achievement, his description for before and after achieved
+		}
+	}
+
+	$res = str_replace('&apos;',"'",$res);
+	$res = str_replace('&amp;',"&",$res);
+	return $res;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+function buildIOSLeaderboard($id,$singularID,$pluralID,$data){
 	global $nonSingularLanguages;
 	$res='';
 	foreach($data[$id] as $k=>$v){
@@ -49,7 +86,7 @@ function getImage($img){
 
 }
 
-function buildAchievement($id,$beforeID,$afterID,$data,$r1=''){
+function buildIOSAchievement($id,$beforeID,$afterID,$data,$r1=''){
 	$res='';
 	list($mode,$none)=explode('_',$id,2);
 	$r2=$r1;
@@ -72,6 +109,9 @@ function buildAchievement($id,$beforeID,$afterID,$data,$r1=''){
     }
     return $res;
 }
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
 $data = array();
 $leaderboards = array();
--- iCanLocalize/parse.php
+++ iCanLocalize/parse.php
@@ -38,34 +38,8 @@ foreach($dom->find('string') as $elem){
 	$data[$elem->attr['label']]=buildMap($elem,$amazonLangMap);
 }
 
-
-function buildAmazon(){
-	global $data, $achievements;
-	$res='';
-	//DisplayTitle_en_US,UnlockedDescription_en_US,LockedDescription_en_US,DisplayTitle_en_GB,UnlockedDescription_en_GB,LockedDescription_en_GB,DisplayTitle_de_DE,UnlockedDescription_de_DE,LockedDescription_de_DE,DisplayTitle_fr_FR,UnlockedDescription_fr_FR,LockedDescription_fr_FR,DisplayTitle_it_IT,UnlockedDescription_it_IT,LockedDescription_it_IT,DisplayTitle_es_ES,UnlockedDescription_es_ES,LockedDescription_es_ES,DisplayTitle_ja_JP,UnlockedDescription_ja_JP,LockedDescription_ja_JP,DisplayTitle_zh_CN,UnlockedDescription_zh_CN,LockedDescription_zh_CN,DisplayTitle_ko_KR,UnlockedDescription_ko_KR,LockedDescription_ko_KR,DisplayTitle_pt_BR,UnlockedDescription_pt_BR,LockedDescription_pt_BR,DisplayTitle_ru_RU,UnlockedDescription_ru_RU,LockedDescription_ru_RU'."\n";
-	$res='AchievementId,unlockedIconId,lockedIconId,pointValue,isHidden';
-	foreach($data[$achievements[0]['title_id']] as $lang=>$v){
-		$res.=',DisplayTitle_'.$lang.',UnlockedDescription_'.$lang.',LockedDescription_'.$lang;
-	}
-
-	//AchievementID,UnlockedIconId,LockedIconId,50,TRUE,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description
-	foreach($achievements as $ach){
-	//	if(!empty($data[$ach['id']])) ;
-		$res .= "\n".$ach['id'].','.$ach['icon'].','.$ach['icon'].','.$ach['points'].','.($ach['hidden']?'TRUE':'FALSE');
-		foreach($data[$ach['title_id']] as $lang=>$title){
-			$locked = $data[$ach['description_id']][$lang];
-			$unlocked = $data[$ach['unlocked_id']][$lang];
-			$res.=',"'.$title.'","'.$unlocked.'","'.$locked.'"'; // add to every achievement, his description for before and after achieved
-		}
-	}
-
-	$res = str_replace('&apos;',"'",$res);
-	$res = str_replace('&amp;',"&",$res);
-	return $res;
-}
-
 $data=expand($data);
-echo buildAmazon();
+echo buildAmazonLeaderboards();
 //print_r($data);
 //echo buildLeaderBoard('leaderboard_completed_levels','level_singular','level_plural',$data);
 //print_r($data);
