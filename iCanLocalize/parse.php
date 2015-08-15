c5f90a77a7e8ca102a778712bce07eed9a1ffba9
Federico Bricker <fbricker@gmail.com>
2015-08-15 15:48:04 -0300

    keep implementing Amazon

--- iCanLocalize/parse.php
+++ iCanLocalize/parse.php
@@ -42,18 +42,25 @@ foreach($dom->find('string') as $elem){
 function buildAmazon(){
 	global $data, $achievements;
 	$res='';
-	$res='AchievementId,unlockedIconId,lockedIconId,pointValue,isHidden,DisplayTitle_en_US,UnlockedDescription_en_US,LockedDescription_en_US,DisplayTitle_en_GB,UnlockedDescription_en_GB,LockedDescription_en_GB,DisplayTitle_de_DE,UnlockedDescription_de_DE,LockedDescription_de_DE,DisplayTitle_fr_FR,UnlockedDescription_fr_FR,LockedDescription_fr_FR,DisplayTitle_it_IT,UnlockedDescription_it_IT,LockedDescription_it_IT,DisplayTitle_es_ES,UnlockedDescription_es_ES,LockedDescription_es_ES,DisplayTitle_ja_JP,UnlockedDescription_ja_JP,LockedDescription_ja_JP,DisplayTitle_zh_CN,UnlockedDescription_zh_CN,LockedDescription_zh_CN,DisplayTitle_ko_KR,UnlockedDescription_ko_KR,LockedDescription_ko_KR,DisplayTitle_pt_BR,UnlockedDescription_pt_BR,LockedDescription_pt_BR,DisplayTitle_ru_RU,UnlockedDescription_ru_RU,LockedDescription_ru_RU'."\n";
-	foreach($achievements as $ach){
-		if(!empty($data[$ach['id']])) print_r($data[$ach['id']]);
-		$res .= $ach['id'].',UnlockedIconId,LockedIconId,'.$ach['points'].','.($ach['hidden']?'TRUE':'FALSE')."\n";
+	//DisplayTitle_en_US,UnlockedDescription_en_US,LockedDescription_en_US,DisplayTitle_en_GB,UnlockedDescription_en_GB,LockedDescription_en_GB,DisplayTitle_de_DE,UnlockedDescription_de_DE,LockedDescription_de_DE,DisplayTitle_fr_FR,UnlockedDescription_fr_FR,LockedDescription_fr_FR,DisplayTitle_it_IT,UnlockedDescription_it_IT,LockedDescription_it_IT,DisplayTitle_es_ES,UnlockedDescription_es_ES,LockedDescription_es_ES,DisplayTitle_ja_JP,UnlockedDescription_ja_JP,LockedDescription_ja_JP,DisplayTitle_zh_CN,UnlockedDescription_zh_CN,LockedDescription_zh_CN,DisplayTitle_ko_KR,UnlockedDescription_ko_KR,LockedDescription_ko_KR,DisplayTitle_pt_BR,UnlockedDescription_pt_BR,LockedDescription_pt_BR,DisplayTitle_ru_RU,UnlockedDescription_ru_RU,LockedDescription_ru_RU'."\n";
+	$res='AchievementId,unlockedIconId,lockedIconId,pointValue,isHidden';
+	foreach($data[$achievements[0]['id']] as $lang=>$v){
+		$res.=',DisplayTitle_'.$lang.',UnlockedDescription_'.$lang.',LockedDescription_'.$lang;
 	}
-//AchievementID,UnlockedIconId,LockedIconId,50,TRUE,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description
+
+	//AchievementID,UnlockedIconId,LockedIconId,50,TRUE,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description,Displayed Achievement Title,Unlocked Achievement Description,Locked Achievement Description
+	foreach($achievements as $ach){
+	//	if(!empty($data[$ach['id']])) ;
+		$res .= "\n".$ach['id'].',UnlockedIconId,LockedIconId,'.$ach['points'].','.($ach['hidden']?'TRUE':'FALSE');
+		foreach($data[$ach['id']] as $lang=>$v){
+			$res.=',Title: '.$lang.',Unlocked: '.$lang.', Locked: '.$lang; // add to every achievement, his description for before and after achieved
+		}
+	}
 	return $res;
 }
 
 $data=expand($data);
-//echo buildAmazon();
-print_r($data);
-
+echo buildAmazon()."\n";
+//print_r($data);
 //echo buildLeaderBoard('leaderboard_completed_levels','level_singular','level_plural',$data);
 //print_r($data);
